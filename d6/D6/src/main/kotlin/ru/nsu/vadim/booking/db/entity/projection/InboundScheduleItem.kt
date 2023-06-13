90b9c3b1c9be81215c4b7568390a8656105bd426
lilvadim <vadimmos2002@gmail.com>
2023-06-13 18:27:10 +0700

    fix

--- d6/D6/src/main/kotlin/ru/nsu/vadim/booking/db/entity/projection/InboundScheduleItem.kt
+++ d6/D6/src/main/kotlin/ru/nsu/vadim/booking/db/entity/projection/InboundScheduleItem.kt
@@ -1,15 +1,12 @@
 package ru.nsu.vadim.booking.db.entity.projection
 
 import com.vladmihalcea.hibernate.type.array.ListArrayType
-import jakarta.persistence.Column
 import org.hibernate.annotations.Type
 import java.time.OffsetTime
 
 interface InboundScheduleItem {
     @get:Type(ListArrayType::class)
     val daysOfWeek: List<Int>
-
-    @get:Column(columnDefinition = "TIME WITH TIME ZONE")
     val timeOfArrival: OffsetTime
     val flightNo: String
     val originAirportCode: String
